87fb17a2cff7865cb7465d665669680cd8899916
carlosemartins97 <carlosemartins97@gmail.com>
2021-11-05 18:27:19 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,23 @@ import { AuthService } from './auth.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular on Fire / Cyro';
-
+export class AppComponent {
   email: string;
   password: string;
 
-  constructor(public authService: AuthService){}
+  constructor(public authService: AuthService) {}
 
-  loginWithEmail(){
+  loginWithEmail() {
     this.authService.loginWithEmail(this.email, this.password);
   }
 
-  loginWithGoogle(){
+  loginWithGoogle() {
     this.authService.loginWithGoogle();
   }
 
-  logout(){
+  logout() {
     this.authService.logout();
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth'; 
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AuthService } from './auth.service';
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
+  imports: [
+    BrowserModule,
     FormsModule,
-    AngularFireModule.initializeApp(
-      {
-        apiKey: "AIzaSyD7eNl5ex9hPVttpyWZmtj_WD0Iw0ZsZ3U",
-        authDomain: "angularlist-c73a7.firebaseapp.com",
-        projectId: "angularlist-c73a7",
-        storageBucket: "angularlist-c73a7.appspot.com",
-        messagingSenderId: "1022061440709",
-        appId: "1:1022061440709:web:8190b7831ec2162c240aac"
-      }
-    ),
-    AngularFireAuthModule
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyCpzV3qMFPI2_BwBZ069YrdgnL_slK4wY8',
+      authDomain: 'aula-angular-1.firebaseapp.com',
+      projectId: 'aula-angular-1',
+      storageBucket: 'aula-angular-1.appspot.com',
+      messagingSenderId: '963139671085',
+      appId: '1:963139671085:web:adc6ef83366f82619dd4a4',
+    }),
+    AngularFireAuthModule,
   ],
-  declarations: [ AppComponent, HelloComponent],
-  bootstrap:    [ AppComponent ],
-  providers: [AuthService]
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [AuthService],
 })
-export class AppModule { }
+export class AppModule {}
